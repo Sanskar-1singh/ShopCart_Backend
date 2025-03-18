fdb0a2df25a6d608091e525cf8c64f50d00546f0
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-03-18 11:27:56 +0530

    template ready

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+const ServerConfig=require('./config');
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded());
+
+app.get('/',(req,res)=>{
+    res.send('pong');
+})
+
+app.listen(ServerConfig.PORT,(ServerConfig.PORT)=>{
+    console.log(`server started at port ${ServerConfig.PORT}`);
+})
