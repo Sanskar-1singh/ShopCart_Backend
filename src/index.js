6d72bf707dd2624b0bfde56af721ef562bbd7ab2
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-03-18 11:33:21 +0530

    added dotenv

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const ServerConfig=require('./config');
+const {ServerConfig}=require('./config');
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded());
@@ -9,6 +9,6 @@ app.get('/',(req,res)=>{
     res.send('pong');
 })
 
-app.listen(ServerConfig.PORT,(ServerConfig.PORT)=>{
+app.listen(ServerConfig.PORT,()=>{
     console.log(`server started at port ${ServerConfig.PORT}`);
 })
