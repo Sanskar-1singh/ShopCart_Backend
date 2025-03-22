ddee87f62fb2596272fd1306418ecda8d0c51a97
Sanskar-1singh <sanskarsingh812@gmail.com>
2025-03-22 10:32:57 +0530

    added auth middlewares

--- src/middlewares/auth-middlewares.js
+++ src/middlewares/auth-middlewares.js
@@ -4,13 +4,14 @@ const {verifyToken}=require('../utils/auth');
 
 
 function isAuthenticated(req,res,next){
-    const token=req.cookies['token'];
+    const token=req.cookies['x-access-token'];
     console.log(token);
     if(!token){
         ErrorReponse.message='Missing JWT token';
         return res.status(StatusCodes.BAD_REQUEST).json(ErrorReponse);
     }
     try {
+       // token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdjZXQxQGdtYWlsLmNvbSIsImlkIjoyNSwiaWF0IjoxNzQyNjE5MTYwLCJleHAiOjE3NDMwNTExNjB9.0dEopWPv9HpnhZEk6dxY5EworB7ANamkPxxteuzEjNw"
         const decodejwttoken=verifyToken(token);
         console.log(decodejwttoken);
     } catch (error) {
@@ -20,8 +21,11 @@ function isAuthenticated(req,res,next){
         return res.status(error.statusCode || StatusCodes.BAD_REQUEST).json(ErrorReponse);
     }
     
+    req.user=decodejwttoken;
     next();
 }
+
+
 
 module.exports={
     isAuthenticated
